4ae802f43cc7d25a2013dce4caf0f564becdf5ab
Andrei Fomin <vfvfns2010@mail.ru>
2023-10-09 12:59:45 +0300

    custom view lsson update

---
+++ CustomViewLesson/app/src/main/java/com/example/customviewlesson/TestVIew.kt
@@ -0,0 +1,31 @@
+package com.example.customviewlesson
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class TestVIew(
+    context: Context,
+    attributeSet: AttributeSet
+) : View(context, attributeSet) {
+    private val paintR = Paint()
+    private val paintC = Paint()
+
+    init {
+        paintR.style = Paint.Style.STROKE
+        paintR.strokeWidth = 5f
+        paintR.color = Color.GRAY
+
+        paintC.style = Paint.Style.FILL
+        paintC.color = Color.RED
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawRect(0f, 0f, width.toFloat(), height.toFloat(), paintR)
+        canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), 100f, paintC)
+    }
+}
